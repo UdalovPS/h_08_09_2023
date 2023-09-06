4418d2418950ec52bedca046221546ca1eb4628f
Udalov Pavel <udalovpavel95@gmail.com>
2023-09-06 23:10:22 +0500

    add one predict view

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -12,5 +12,27 @@ templates = Jinja2Templates(directory='templates')
 
 @app.get("/", response_class=HTMLResponse)
 async def get_index_html(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse("test.html", {"request": request})
 
+
+@app.post("/predict_one")
+async def predict_one_person(name = Form(),
+                             age = Form(),
+                             some = Form(),
+                             sel = Form(),
+                             price = Form(),
+                             check_1: bool = Form(False),
+                             check_2: bool = Form(False),
+                             flexRadioDefault = Form()):
+
+    form_dict = {
+        "name": name,
+        "age": age,
+        "some": some,
+        "select": sel,
+        "price": price,
+        "check_1": check_1,
+        "check_2": check_2,
+        "flexRadioDefault": flexRadioDefault
+    }
+    return form_dict
